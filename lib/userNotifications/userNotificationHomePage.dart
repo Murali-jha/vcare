6168c84e6260352cf7fc0a9213302e28d01e8471
Murali-jha <muralijha01@gmail.com>
2021-06-21 13:59:52 +0530

    added notification feature

--- lib/userNotifications/userNotificationHomePage.dart
+++ lib/userNotifications/userNotificationHomePage.dart
@@ -4,6 +4,7 @@ import 'package:e_shop/Widgets/loadingWidget.dart';
 import 'package:e_shop/Widgets/myDrawer.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserNotificationsHomePage extends StatefulWidget {
   @override
@@ -11,6 +12,37 @@ class UserNotificationsHomePage extends StatefulWidget {
 }
 
 class _UserNotificationsHomePageState extends State<UserNotificationsHomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkDialogIntroShown();
+  }
+
+  Future checkDialogIntroShown() async {
+    SharedPreferences userNotificationsHomePageDialog = await SharedPreferences.getInstance();
+    bool _seen = (userNotificationsHomePageDialog.getBool('seenUserNotificationsHomePageDialog') ?? false);
+
+    if (_seen) {
+
+
+    }
+    else{
+      await userNotificationsHomePageDialog.setBool('seenUserNotificationsHomePageDialog', true);
+      //showAlertDialog(context);
+      showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => CustomAlertDialog(
+          title: "Hey ${EcommerceApp.sharedPreferences.getString(EcommerceApp.userName)} !",
+          desc: "Here you will find all the notifications and reminders. ",
+        ),
+      );
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,5 +93,99 @@ class _UserNotificationsHomePageState extends State<UserNotificationsHomePage> {
             );
           },
         ));
+  }
+}
+
+class CustomAlertDialog extends StatelessWidget {
+
+  final String title,desc;
+
+  CustomAlertDialog({ this.title, this.desc,});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16)
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: dialogContent(context),
+    );
+  }
+
+  dialogContent(BuildContext context){
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.only(
+              top: 100.0,
+              bottom: 16.0,
+              left: 16.0,
+              right: 16.0
+          ),
+          margin: EdgeInsets.only(
+              top: 16.0
+          ),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(17),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10.0,
+                  offset: Offset(0.0,10.0),
+                )
+              ]
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black,fontFamily: "Poppins"
+                ),
+              ),
+              SizedBox(height: 24.0,),
+              Text(
+                desc,
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,fontFamily: "Poppins"
+                ),
+              ),
+              SizedBox(height: 24.0,),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: FlatButton(
+                  color: Colors.green,
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  child: Text("Confirm",style: TextStyle(color: Colors.white,fontFamily: "Poppins"),),
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
+        Positioned(
+            top: 0.0,
+            left: 16.0,
+            right: 16.0,
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 50.0,
+              backgroundImage: AssetImage("assets/gifs/7t4e.gif"),
+            )
+        )
+      ],
+    );
   }
 }
